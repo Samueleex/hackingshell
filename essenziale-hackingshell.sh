64145658270e29f704361e8868838a12d77a078b
Samuele <115916720+Samueleex@users.noreply.github.com>
2024-04-02 09:12:30 +0000

    Add files via upload

---
+++ essenziale-hackingshell.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+clear
+echo '     '
+echo '              (   (          '
+echo '   (    (     )\ ))\ )  (    '
+echo ' ( )\   )\   (()/(()/(  )\   '
+echo ' )((_|(((_)(  /(_))(_)|((_)  '
+echo '((_)_ )\ _ )\(_))(_)) )\___  '
+echo ' | _ )(_)_\(_) __|_ _((/ __| '
+echo ' | _ \ / _ \ \__ \| | | (__  '
+echo ' (___//_/ \_\|___/(__| (___| '
+echo ' )\ )  *   )      )\ ) )\ )  '
+echo '(()/(` )  /(   ( (()/((()/(  '
+echo ' /(_))( )(_))  )\ /(_))/(_)) '
+echo '(_)) (_(_())_ ((_|_))_(_))_| '
+echo '/ __||_   _| | | | |_ | |_   '
+echo '\__ \  | | | |_| | __|| __|  '
+echo '|___/  |_|  \___/|_|  |_|    '
+echo 'Andiamo ad installare componenti base: neofetch, python, figlet, tmate, apache2, netcat, ftp e php.'                            
+                                                                        
+sleep 1
+echo '20%'
+sleep 1
+echo '40%'
+sleep 1
+echo '60&'
+sleep 1
+echo '80%'
+sleep 3
+echo '100%'
+sleep 2
+sudo apt update
+sudo apt upgrade -y
+sudo apt install neofetch -y
+sudo apt install python -y
+sudo apt install python2 -y
+sudo apt install python3 -y
+sudo apt install figlet -y
+sudo apt install tmate -y
+sudo apt install nmap -y
+sudo apt install apache2 -y
+sudo apt install netcat -y
+sudo apt install ftp -y
+sudo apt install php -y
+sudo apt update
+sudo apt upgrade -y
+clear
+echo '           )     )        (                '
+echo ' (      ( /(  ( /( (      )\ )             '
+echo ' )\ )   )\()) )\()))\ )  (()/( (           '
+echo '(()/(  ((_)\ ((_)\(()/(   /(_)))\          '
+echo ' /(_))_  ((_)  ((_)/(_))_(_)) ((_)         '
+echo '(_)) __|/ _ \ / _ (_)) __| |  | __|        '
+echo '  | (_ | (_) | (_) || (_ | |__| _|         '
+echo '   \)__|\___/ \___/  \_)_(____|__)|        '
+echo ' ( /(  (       (    ( /( )\ ) ( /( (       '
+echo ' )\()) )\      )\   )\()|()/( )\()))\ )    '
+echo '((_)((((_)(  (((_)|((_)\ /(_)|(_)\(()/(    '
+echo ' _((_)\ _ )\ )\___|_ ((_|_))  _((_)/(_))_  '
+echo '| || (_)_\(_|(/ __| |/ /|_ _|| \| (_)) __| '
+echo '| __ |/ _ \  | (__  ' <  | | | .` | | (_ | '
+echo '|(||_/_/ )_\  \(__|_(\_\|___||_|\_|  \___| '
+echo ' )\ ) ( /(     )\ ) )\ )                   '
+echo '(()/( )\())(  (()/((()/(                   '
+echo ' /(_)|(_)\ )\  /(_))/(_))                  '
+echo '(_))  _((_|(_)(_)) (_))                    '
+echo '/ __|| || | __| |  | |                     '
+echo '\__ \| __ | _|| |__| |__                   '
+echo '|___/|_||_|___|____|____|                  '
+                                           
+sleep 1
+echo "Benvenuto nella Google HackingShell!"
+echo "Ora puoi hackerare da qualsiasi dispositivo usando il tuo browser!!!"
+echo "Per l'ssh dovrai avviarlo in una nuova shell: ssh (codice)"
+echo "Il tempo di utilizzo è di 50 ore a settimana!"
+echo "- Samueleex"
+sleep 15
+clear
+figlet "caricameto..."
+sleep 5
+tmate
---
+++ osint-hackingshell.sh
@@ -0,0 +1,96 @@
+sudo apt install git -y
+clear
+echo "Questo è uno script bash che installa dei tools utili per l'OSINT su Google Shell!"
+sleep 2
+echo '   '
+echo '   '
+echo '   '
+echo '    )  (   (       )                              
+echo ' ( /(  )\ ))\ ) ( /(   *   )      )         (     '
+echo ' )\())(()/(()/( )\())` )  /(   ( /(         )\    '
+echo '((_)\  /(_))(_)|(_)\  ( )(_))  )\())(    ( ((_|   '
+echo '  ((_)(_))(_))  _((_)(_(_())  (_))/ )\   )\ _ )\  '
+echo ' / _ \/ __|_ _|| \| ||_   _|  | |_ ((_) ((_) ((_) '
+echo '| (_) \__ \| | | .` |  | |    |  _/ _ \/ _ \ (_-< '
+echo ' \___/|___/___||_|\_|  |_|     \__\___/\___/_/__/ '
+echo '   '
+echo 'Scelti da samueleex'                                                                                     
+sleep 1
+echo "hashcracker (by Samueleex)"
+echo "TgTools (by Samueleex)"
+echo "Shodan"
+echo "metasploit-framework"
+echo "sherlock"
+echo "osintgram"
+echo "h8mail"
+echo "recon-ng"
+echo "Email-Harvester"
+echo "PhoneInfoga"
+echo "DoxPhone"
+echo "nmap"
+echo "photon"
+echo "vector"
+echo "Se avete altre idee lasciate una risposta sulla discussione nel forum (EHF)!"
+sleep 5
+echo "installazione di hashcracker (by Samueleex)..."
+git clone https://github.com/Samueleex/hashcracker
+echo "installazione di TgTools (by Samueleex)..."
+git clone https://github.com/Samueleex/TgTools
+cd TgTools
+chmod +x * && python3 setup.py
+cd
+cd hackingshell
+echo "installazione di shodan..."
+sudo apt install python3-shodan -y
+echo "installing metasploit-framework..."
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+chmod 755 msfinstall && \
+./msfinstall
+echo "installing sherlock..."
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock
+sudo apt install python3-numpy -y
+python3 -m pip install -r requirements.txt
+cd
+cd hackingshell
+echo "installazione di osintgram..."
+git clone https://github.com/Datalux/Osintgram.git
+cd Osintgram
+python3 -m venv venv
+cd
+cd hackingshell
+echo "installazione h8mail"
+pip3 install h8mail
+echo "installing recon-ng..."
+sudo apt install recon-ng -y
+echo "installazione Email-Harvester..."
+sudo apt install python3-termcolor -y
+sudo apt install python3-colorama -y
+git clone https://github.com/maldevel/EmailHarvester
+cd EmailHarvester
+pip install -r requirements.txt
+cd
+cd hackingshell
+echo "installazione PhoneInfoga..."
+git clone https://github.com/la-deep-web/Phoneinfoga
+cd Phoneinfoga
+python3 -m pip install -r requirements.txt
+cd
+cd hackingshell
+echo "installazione DoxPhone..."
+git clone https://github.com/LCA-HACK/DoxPhone
+cd DoxPhone
+python2 instalacion.py
+cd
+cd hackingshell
+echo "installazione di nmap..."
+sudo apt install nmap -y
+echo "photon"
+git clone https://github.com/s0md3v/Photon.git
+cd Photon
+sudo apt install docker -y
+docker build -t photon
+docker run -it --name photon photon:latest -u google.com
+cd
+cd hackingshell
+echo "Installazione terminata!"
